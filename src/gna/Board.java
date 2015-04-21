45ac05f2911ce15402eac11e3a4ae5da679e3c8f
Louis Roebben <louis.roebben@student.kuleuven.be>
2015-04-21 16:33:34 +0200

    wip v2

--- src/gna/Board.java
+++ src/gna/Board.java
@@ -1,13 +1,17 @@
 package gna;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 
 public class Board
 {
+	private int[][] iaSpel;
 	// construct a board from an N-by-N array of tiles
 	public Board( int[][] tiles )
 	{
-		throw new RuntimeException("not implemented"); // TODO
+		iaSpel = tiles;
+		//throw new RuntimeException("not implemented"); // TODO
 	}
 	
 	// return number of blocks out of place
@@ -19,21 +23,71 @@ public class Board
 	// return sum of Manhattan distances between blocks and goal
 	public int manhattan()
 	{
-		throw new RuntimeException("not implemented"); // TODO
+		int iCounter = 0;
+		for(int x; x < iaSpel.length; x++)
+		{
+			for(int y; y < iaSpel.length; y++)
+			{
+				if(iaSpel[x][y] == (x*iaSpel.length + y))
+				{
+						continue;
+					} else {
+						iCounter += Math.abs((iaSpel[x][y] / iaSpel.length) - x);
+						iCounter += Math.abs((iaSpel[x][y] % iaSpel.length) - y);
+				}
+			}
+		}
+		return iCounter;
 	}
-	
+
 	// does this board position equal y
 	public boolean equals(Object y)
 	{
-		throw new RuntimeException("not implemented"); // TODO
+		return y == this;
 	}
 	
 	// return a Collection of all neighboring board positions
 	public Collection<Board> neighbors()
 	{
-		throw new RuntimeException("not implemented"); // TODO
+		Collection<Board> idxCollection = new ArrayList<Board>();
+
+		int[] iaNull = fncFindLoc(0);
+		if (iaNull[0] != iaSpel.length)
+		{
+			// een rechter buur
+			int[][] iaNeighbour = iaSpel;
+			iaNeighbour[iaNull[0]][iaNull[1]] = iaNeighbour[iaNull[0]+1][iaNull[1]];
+			iaNeighbour[iaNull[0+1]][iaNull[1]] = 0;
+			idxCollection.add(new Board(iaNeighbour));
+		}
+		if (iaNull[0] != 0)
+		{
+			//een linker buur
+			int[][] iaNeighbour = iaSpel;
+			iaNeighbour[iaNull[0]][iaNull[1]] = iaNeighbour[iaNull[0]-1][iaNull[1]];
+			iaNeighbour[iaNull[0-1]][iaNull[1]] = 0;
+			idxCollection.add(new Board(iaNeighbour));
+		}
+		if (iaNull[1] != iaSpel.length)
+		{
+			//een onder buur
+			int[][] iaNeighbour = iaSpel;
+			iaNeighbour[iaNull[0]][iaNull[1]] = iaNeighbour[iaNull[0]][iaNull[1-1]];
+			iaNeighbour[iaNull[0]][iaNull[1-1]] = 0;
+			idxCollection.add(new Board(iaNeighbour));
+		}
+		if(iaNull[1] != 0)
+		{
+			//een boven buur
+			int[][] iaNeighbour = iaSpel;
+			iaNeighbour[iaNull[0]][iaNull[1]] = iaNeighbour[iaNull[0]][iaNull[1+1]];
+			iaNeighbour[iaNull[0]][iaNull[1+1]] = 0;
+			idxCollection.add(new Board(iaNeighbour));
+		}
+
+		return idxCollection;
 	}
-	
+
 	// return a string representation of the board
 	public String toString()
 	{
@@ -44,6 +98,21 @@ public class Board
 	public boolean isSolvable()
 	{
 		throw new RuntimeException("not implemented"); // TODO
+	}
+
+	private int[] fncFindLoc(int iFind)
+	{
+		for(int x; x < iaSpel.length; x++)
+		{
+			for (int y; y < iaSpel.length; y++)
+			{
+				if (iaSpel[x][y] == iFind)
+				{
+					int[] iaReturn = {x,y};
+				}
+			}
+		}
+		return iaReturn;
 	}
 }
 
